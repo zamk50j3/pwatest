1b24e7b461a6ea71177e04bcd2c77cf6bd604935
zamk50j3 <66684713+zamk50j3@users.noreply.github.com>
2021-08-25 12:28:56 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,27 +1,27 @@
-// キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    '/zamk50j3.github.io/pwatest/',
-];
+// // キャッシュファイルの指定
+// var CACHE_NAME = 'pwa-sample-caches';
+// var urlsToCache = [
+//     '/zamk50j3.github.io/pwatest/',
+// ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+// // インストール処理
+// self.addEventListener('install', function(event) {
+//     event.waitUntil(
+//         caches
+//             .open(CACHE_NAME)
+//             .then(function(cache) {
+//                 return cache.addAll(urlsToCache);
+//             })
+//     );
+// });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
-});
+// // リソースフェッチ時のキャッシュロード処理
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//         caches
+//             .match(event.request)
+//             .then(function(response) {
+//                 return response ? response : fetch(event.request);
+//             })
+//     );
+// });
